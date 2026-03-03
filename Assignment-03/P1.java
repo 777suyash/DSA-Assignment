b81f0e474369933118e9d1e87c71ab9bd5d58781
Suyash Bhosale <suyashbhosale1305@gmail.com>
2026-03-04 00:53:48 +0530

    assignment 3 added

--- Assignment-03/P1.java
+++ Assignment-03/P1.java
@@ -2,22 +2,22 @@ public class P1 {
     public static void main(String[] args) {
         int n = 4;
         for(int i = 1; i <= n; i++){
-            for (int j = 1; j <= n-i; j++) {
-                System.err.print("  ");
+            for(int j = 1; j <= n - i; j++){
+                System.out.print(" ");
             }
-            for (int j = 1; j <= (2*i)-1; j++) {
-                System.err.print("* ");
+            for(int j = 1; j <= i; j++){
+                System.out.print(j+" ");
             }
-            System.err.println();
+            System.out.println();
         }
-        // for(int i = n - 1; i >= 1; i--){
-        //     for (int j = 1; j <= n - i; j++) {
-        //         System.err.print("  ");
-        //     }
-        //     for (int j = 1; j <= 2* i - 1; j++) {
-        //         System.err.print("* ");
-        //     }
-        //     System.err.println();
-        // }
+        for(int i = n - 1; i >= 1; i--){
+            for(int j = 1; j <= n - i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
     }
 }
---
+++ Assignment-03/P5.java
@@ -0,0 +1,16 @@
+public class P5 {
+    public static void main(String[] args) {
+            int n = 6;
+            for(int i = 0; i < n; i++){
+                int num = 1;
+                for (int j = 1; j <= n-i; j++) {
+                    System.out.print(" ");
+                }
+                for (int j =0; j<= i; j++) {
+                    System.out.print(num+" ");
+                    num = num * (i-j)/(j+1);
+                }
+                System.out.println();
+            }
+    }
+}
